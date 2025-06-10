44658c7dc688b4511eab0375f1b618d30ed2be8b
milad <milad.h.b1382@gmail.com>
2025-06-11 01:43:22 +0330

    scrol login

--- components/LoginForm/LoginStep1Form.tsx
+++ components/LoginForm/LoginStep1Form.tsx
@@ -53,7 +53,7 @@ const LoginStep1Form: React.FC<LoginStep1FormProps> = ({ setOpen }) => {
   };
 
   return (
-    <div className="w-full mb-[100px]">
+    <div className="w-full mb-[100px] overflow-y-scroll">
       <DialogTitle className="text-[32px] font-bold p-0 text-white w-full text-center">
         ورود به حساب کاربری
       </DialogTitle>
